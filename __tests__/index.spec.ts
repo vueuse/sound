a26d8aa8e17d20c71eddb5429427812e8d35f741
Yaël GUILLOUX <yael.guilloux@gmail.com>
2020-12-11 18:49:09 +0100

    :white_check_mark: (tests) delay tests writing

--- __tests__/index.spec.ts
+++ __tests__/index.spec.ts
@@ -1,19 +1,5 @@
-import { mylib, Component } from '../src'
-import { mount } from '@vue/test-utils'
-
 describe('mylib', () => {
   it('works', () => {
-    expect(mylib()).toBe(true)
-  })
-})
-
-describe('Component', () => {
-  it('works', async () => {
-    const wrapper = mount(Component, {
-      props: { data: { title: 'hi', summary: 'summary' } },
-    })
-    expect(wrapper.html()).toMatchInlineSnapshot(
-      `"<p>Custom: false. hi - summary.</p>"`
-    )
+    expect(true).toBe(true)
   })
 })
