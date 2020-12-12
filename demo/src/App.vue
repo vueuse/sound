dd83d7620878591f8a059aa39f6ce749f880a144
Yaël GUILLOUX <yael.guilloux@gmail.com>
2020-12-12 15:02:42 +0100

    :pencil: (demo) preparing demo app

---
+++ demo/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="flex flex-col justify-center">
+    <div class="flex justify-center align-center">
+      <h1 class="m-6 text-4xl font-bold" @click="play">
+        🔊&nbsp;vue-use-sound
+      </h1>
+    </div>
+  </div>
+</template>
+
+<script>
+import useSound from 'vue-use-sound'
+import Button from './assets/button.wav'
+
+export default {
+  name: 'App',
+  setup() {
+    const [play] = useSound(Button)
+
+    return {
+      play,
+    }
+  },
+}
+</script>
---
+++ demo/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ['./src/**/*.{vue,js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
