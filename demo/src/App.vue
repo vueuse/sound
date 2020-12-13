10fe20c23b6eb1f23ee04a86a3883bfb58334465
Yaël GUILLOUX <yael.guilloux@gmail.com>
2020-12-13 16:09:49 +0100

    :sparkles: (demo) back button and credits

--- demo/src/App.vue
+++ demo/src/App.vue
@@ -5,14 +5,35 @@
         class="m-6 text-4xl font-bold cursor-pointer select-none"
         @click="play"
       >
-        🔊&nbsp;vue-use-sound
+        🎺&nbsp;vue-use-sound
       </h1>
     </div>
 
-    <intro id="#hover" class="mt-12" />
     <checkboxes id="#checkboxes" class="mt-12" />
+    <intro id="#hover" class="mt-12" />
     <increase-pitch id="#pitch" class="mt-12" />
     <drum-machine id="#drums" class="mt-12" />
+
+    <div class="flex flex-col items-center justify-center">
+      <a href="https://github.com/Tahul/vue-use-sound" class="mt-12 mb-6">
+        ↩️&nbsp;&nbsp;Bring me back to GitHub
+      </a>
+
+      <p class="mb-6">
+        This package has been written by
+        <a href="https://twitter.com/yaeeelglx" target="_blank"
+          >Yaël Guilloux</a
+        >
+        and is deeply inspired from
+        <a href="https://twitter.com/JoshWComeau" target="_blank"
+          >Josh W. Comeau</a
+        >
+        work,
+        <a href="https://github.com/joshwcomeau/use-sound" target="_blank"
+          >use-sound</a
+        >.
+      </p>
+    </div>
   </div>
 </template>
 
@@ -36,3 +57,9 @@ export default {
   },
 }
 </script>
+
+<style scoped>
+a {
+  @apply font-bold transition-colors duration-100 hover:text-pink-500;
+}
+</style>
