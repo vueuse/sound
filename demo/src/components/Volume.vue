1678df84bd02ac585db210a1f1996afa39aa5806
Yaël GUILLOUX <yael.guilloux@gmail.com>
2021-01-13 16:28:37 +0100

    :sparkles: (demo) volume
    implement random volume demo

---
+++ demo/src/components/Volume.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="flex flex-col justify-center">
+    <div class="flex justify-center mb-6">
+      <Button @click="handleClick">🎛️</Button>
+    </div>
+
+    <code-heading> Play the sound with a random volume value. </code-heading>
+
+    <code-block :codeText="CodeText" />
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+import useSound from 'vue-use-sound'
+import glugSfx from '../assets/glug.mp3'
+import CodeText from '../examples/volume'
+import Button from './Button.vue'
+import CodeBlock from './CodeBlock.vue'
+import CodeHeading from './CodeHeading.vue'
+
+export default {
+  components: { CodeHeading, CodeBlock, Button },
+  setup() {
+    const volume = ref(0.1)
+
+    const [play] = useSound(glugSfx, {
+      volume,
+      interrupt: true,
+    })
+
+    const handleClick = () => {
+      volume.value = parseFloat(Math.random().toFixed(2))
+
+      play()
+    }
+
+    return {
+      handleClick,
+      CodeText,
+    }
+  },
+}
+</script>
