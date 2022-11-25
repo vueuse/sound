3c4978bb07c3ef9638c0054e0e4a90923aca982c
Yaël Guilloux <yael.guilloux@gmail.com>
2022-11-25 15:56:13 +0100

    fix(demo): fix horizontal viewport

--- demo/components/content/Button.vue
+++ demo/components/content/Button.vue
@@ -15,7 +15,7 @@ export default {
 </script>
 
 <template>
-  <button class="rounded-full bg-indigo-400 p-4 text-6xl focus:outline-none focus:ring" v-bind="props">
+  <button class="rounded-full bg-indigo-400 p-4 text-2xl md:text-6xl sm:text-4xl focus:outline-none focus:ring" v-bind="props">
     <span role="img" :aria-label="props.label">
       <slot />
     </span>
