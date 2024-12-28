57b7d13fc66633db05e8e86c416e899ec7098ccf
Yaël Guilloux <yael.guilloux@gmail.com>
2024-12-28 17:40:34 +0100

    chore(lint): update lint config & add vscode settings

---
+++ eslint.config.js
@@ -0,0 +1,20 @@
+const antfu = require('@antfu/eslint-config').default;
+
+module.exports = antfu({
+  ignores: ['dist/**/*', '.output/**/*', '.nuxt/**/*', '*.css', '*.md'],
+  stylistic: {
+    semi: true,
+    comma: 'all',
+  },
+  rules: {
+    'antfu/if-newline': 'off',
+    'new-cap': 'off',
+    'vue/static-class-names-order': 'off',
+    'vue/multi-word-component-names': 'off',
+    'vue/require-component-is': 'off',
+    'vue/no-multiple-template-root': 'off',
+    'vue/no-v-text-v-html-on-component': 'off',
+    'unused-imports/no-unused-vars': 'off',
+    'style/semi': ['error', 'never'],
+  },
+})
